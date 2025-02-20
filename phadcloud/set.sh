d098078474e9c7eab699e8bc8bed99e0adbd8318
SpatLyu <lyu.geosocial@gmail.com>
2025-02-20 22:37:13 +0800

    update shell script

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -3,3 +3,4 @@ mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-bas
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-spdep r-xml r-ncdf4 -y
 Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 Rscript - e "install.packages('terra', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
+Rscript - e "install.packages('spEDM_1.5.tar.gz', repos = NULL, type = 'source')"
