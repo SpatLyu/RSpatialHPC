a559fd2ede88132942001ad5bc60fdfae1630806
SpatLyu <lyu.geosocial@gmail.com>
2025-02-24 23:14:17 +0800

    update rpkgs setting script

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -1,6 +1,7 @@
 conda install mamba -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.4.2 r-essentials r-devtools r-languageserver -y
-mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-spdep r-xml r-ncdf4 -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml r-ncdf4 r-fasterize -y
 Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 Rscript - e "install.packages('terra', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 Rscript - e "install.packages('spEDM_1.5.tar.gz', repos = NULL, type = 'source')"
