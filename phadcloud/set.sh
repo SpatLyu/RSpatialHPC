3835859529be6e1c1b6e81d0466a0554b5b2cedd
SpatLyu <lyu.geosocial@gmail.com>
2025-04-12 15:20:58 +0800

    update r-base version

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -1,7 +1,7 @@
 conda create -n rspatial
 conda activate rspatial
 conda install mamba -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
-mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.4.2 r-essentials r-devtools r-languageserver -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.4.3 r-essentials r-devtools r-languageserver -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml r-ncdf4 r-fasterize r-writexl -y
 Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
