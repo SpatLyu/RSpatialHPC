30a1fc33acf0781af07c3588af3ff1dd83b4321d
SpatLyu <lyu.geosocial@gmail.com>
2025-04-12 16:43:37 +0800

    updata shell script

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -2,8 +2,8 @@ conda create -n rspatial
 conda activate rspatial
 conda install mamba -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.4.3 r-essentials r-devtools r-languageserver -y
-mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep r-lwgeom -y
-mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml r-ncdf4 r-fasterize r-writexl -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml r-plot3d r-ncdf4 r-fasterize r-writexl -y
 # mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-lwgeom -y
 # Rscript - e "install.packages('tmap', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 # Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
