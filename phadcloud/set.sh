d6d9f5482155af22cdde996e35d73e0e5f3592fb
SpatLyu <lyu.geosocial@gmail.com>
2025-04-12 15:43:12 +0800

    update shell script

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -2,9 +2,11 @@ conda create -n rspatial
 conda activate rspatial
 conda install mamba -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.4.3 r-essentials r-devtools r-languageserver -y
-mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep r-lwgeom -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml r-ncdf4 r-fasterize r-writexl -y
-Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
-Rscript - e "install.packages('terra', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
-Rscript - e "install.packages('sgsR', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
-Rscript - e "install.packages('spEDM_1.6.tar.gz', repos = NULL, type = 'source')"
+# mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-lwgeom -y
+# Rscript - e "install.packages('tmap', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
+# Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
+# Rscript - e "install.packages('terra', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
+# Rscript - e "install.packages('sgsR', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
+# Rscript - e "install.packages('spEDM_1.6.tar.gz', repos = NULL, type = 'source')"
