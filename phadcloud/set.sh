64529e7406e2d1d01106344acd6021cc4fbf8e91
SpatLyu <lyu.geosocial@gmail.com>
2025-04-21 21:48:15 +0800

    update shell

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -1,7 +1,7 @@
-conda create -n rspatial
+conda create -n rspatial -y
 conda activate rspatial
 conda install mamba -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
-mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.4.3 r-essentials r-devtools r-languageserver -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-base=4.5.0 r-essentials r-devtools r-languageserver -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-sf r-terra r-spatialreg r-spdep -y
 mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml r-plot3d r-ncdf4 r-fasterize r-writexl -y
 # mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-lwgeom -y
@@ -10,3 +10,9 @@ mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml
 # Rscript - e "install.packages('terra', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 # Rscript - e "install.packages('sgsR', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 # Rscript - e "install.packages('spEDM_1.6.tar.gz', repos = NULL, type = 'source')"
+
+conda create -n pyspatial python=3.11 -y
+conda activate pyspatial
+conda install mamba -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge pysal ipykernel -y
+mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge openpyxl xlwt xlrd ruptures mpart -y
