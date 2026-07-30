23fb995183275192f8765dbd2f3fdeaf05413cee
SpatLyu <lyu.geosocial@gmail.com>
2026-07-31 00:00:01 +0800

    bump version number

--- phadcloud/set.sh
+++ phadcloud/set.sh
@@ -9,7 +9,7 @@ mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge r-xml
 # Rscript - e "install.packages('RcppArmadillo', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 # Rscript - e "install.packages('terra', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
 # Rscript - e "install.packages('sgsR', repos = 'https://mirrors.bfsu.edu.cn/CRAN/', dep = TRUE)"
-# Rscript - e "install.packages('spEDM_1.8.tar.gz', repos = NULL, type = 'source')"
+# Rscript - e "install.packages('spEDM_1.13.tar.gz', repos = NULL, type = 'source')"
 
 conda create -n pyspatial python=3.12 -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge -y
 conda activate pyspatial
@@ -20,7 +20,7 @@ mamba install -c https://mirrors.sustech.edu.cn/anaconda/cloud/conda-forge openp
 # build r pkgs in phadcloud
 # srun --partition=INTEL_8581 --nodes=1 --ntasks=1 --time=1:00:00 --pty bash
 
-# install.packages('spEDM_1.12.tar.gz', repos = NULL, type = 'source')
+# install.packages('spEDM_1.13.tar.gz', repos = NULL, type = 'source')
 # install.packages('tEDM_1.3.tar.gz', repos = NULL, type = 'source')
 
 # run r scripts interactively
